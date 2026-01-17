9c34737528cd2ac3b796baf6b0f2a12dd86f892d
Felipe <felipexz.backup@gmail.com>
2026-01-17 11:51:38 -0300

    Fix Firebase import and prepare deploy

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0wFY0xRmdljuIK-OYGq-BJ13mE_AnEok",
+  apiKey: "AIzaSyC0wFY0xRmdIuIK-OYGq-BJ13mE_AnEoK",
   authDomain: "elite-99c81.firebaseapp.com",
   projectId: "elite-99c81",
   storageBucket: "elite-99c81.firebasestorage.app",
   messagingSenderId: "21060621915",
-  appId: "1:21060621915:web:66e197fbfb925a01d1917e"
+  appId: "1:21060621915:web:66e197bfb925a01d1917e"
 };
 
 const app = initializeApp(firebaseConfig);
